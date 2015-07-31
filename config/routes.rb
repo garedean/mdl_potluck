a1cbd2adab116861e9d94d98aaeecb16a0a05675
Michael Yanchar <michael.yanchar@gmail.com>
2015-07-31 10:49:12 -0700

    Added add button to new ingredient summary page. Adds new ingredient object without an assigned location_id.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/home/admin' => 'home#admin'
 
+  resources :ingredients
+
   resources :categories do
     resources :ingredients
   end

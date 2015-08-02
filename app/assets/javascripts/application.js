a91908ad241c0fee9956d3491487ca020fa7f7b8
myanchar <michael.yanchar@gmail.com>
2015-08-02 11:05:38 -0700

    Fixed page redirects and added a back button to location and category navigation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,12 @@ $(function() {
       $(this).delay(5000).fadeOut();
    });
 });
+
+$(document).ready(function() {
+    var page = $('#smoothScroll');  // set to the main content of the page   
+    $(window).mousewheel(function(event, delta, deltaX, deltaY){
+        if (delta < 0) page.scrollTop(page.scrollTop() + 65);
+        else if (delta > 0) page.scrollTop(page.scrollTop() - 65);
+        return false;
+    })
+});

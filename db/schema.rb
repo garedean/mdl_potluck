210d2df223468a2b93b75bee7bf111cbaf77b420
garedean <garrettdolson@gmail.com>
2015-08-03 14:15:25 -0700

    Add Ingredient actions #add_to_cart and #remove_from_cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803201819) do
+ActiveRecord::Schema.define(version: 20150803203711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
   end
+
+  add_index "carts", ["user_id"], name: "index_carts_on_user_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -62,5 +65,6 @@ ActiveRecord::Schema.define(version: 20150803201819) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "carts", "users"
   add_foreign_key "ingredients", "carts"
 end

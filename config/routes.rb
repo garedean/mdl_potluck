43aff5c4c286d95035c50a8cfc437982206a402e
garedean <garrettdolson@gmail.com>
2015-08-03 15:11:44 -0700

    Add Cart#empty_cart and Cart#use_all_items

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   get '/ingredients/expiring', to: 'ingredients#expiring_soon'
   get '/ingredients/unarranged', to: 'ingredients#unarranged'
 
-  post '/ingredients/:id/add_to_cart', to: 'ingredients#add_to_cart', as: "add_to_cart"
-  post '/ingredients/:id/remove_from_cart', to: 'ingredients#remove_from_cart', as: "remove_from_cart"
-
-
 
   resources :ingredients
   resources :locations
@@ -23,4 +19,16 @@ Rails.application.routes.draw do
   resources :locations do
     resources :ingredients
   end
+
+  # cart
+  # **************************
+
+  # add item
+  post '/ingredients/:id/add_to_cart', to: 'ingredients#add_to_cart', as: "add_to_cart"
+  # remove item
+  post '/ingredients/:id/remove_from_cart', to: 'ingredients#remove_from_cart', as: "remove_from_cart"
+  # use all items
+  post '/cart/use_all', to: 'carts#use_all_items', as: "use_all_items"
+  # empty all items
+  post '/cart/empty',   to: 'carts#empty_cart',     as: "empty_cart"
 end

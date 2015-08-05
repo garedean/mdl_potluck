3c055c914e97f73e76d4a38b2c3e91c715a2a2e9
Michael Yanchar <michael.yanchar@gmail.com>
2015-08-05 16:30:36 -0700

    Created admin page with links to functions and summary of database values.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
   before_filter :authenticate_user!, except: [:home]
 
+  def admin
+  	@ingredients = Ingredient.all()
+  	@categories = Category.all()
+  	@locations = Location.all()
+  	@users = User.all()
+  end
+
   def activate
   	@user = User.find(params[:id])
   	if @user.update_attribute(approved: true)

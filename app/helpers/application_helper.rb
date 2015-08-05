a2d7f93bb83594cd2d878d8e40a6dac0264ab6fd
garedean <garrettdolson@gmail.com>
2015-08-05 11:28:27 -0700

    Move #hierarchy link from Category model to ApplicationHelpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  # this app has multiple levels of hierarchical user selection. For instance, when adding a food item, the user must go through multiple levels of food categories to find the desired food to add. List items for each hierarchy follow a pattern: if the item has children assigned to it in the db, then it should link to the next page listing all of the children locations/categories. If it does not have children, go to another page
+
+  def hierarchy_link(object, link_type = nil, category, classes)
+    has_children = object.children.any?
+
+    if link_type == :store
+      if has_children
+        link_to object.name, store_category_path(object), class: classes
+      else
+        link_to object.name, new_store_category_ingredient_path(category), class: classes
+      end
+    elsif link_type == :location
+      if has_children
+        link_to object.name, location_path(object), class: classes
+      else
+        link_to object.name, location_ingredients_path(object), class: classes
+      end
+    # standard 'prepare' category hierarchy link
+    else
+      if has_children
+        link_to object.name, category_path(object), class: classes
+      else
+        link_to object.name, category_ingredients_path(object), class: classes
+      end
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,30 +1,6 @@
 class Category < ActiveRecord::Base
-	# include routes for use in #hierarchy_link
-	include Rails.application.routes.url_helpers
-
 	has_many :children, class_name: "Category", foreign_key: "parent_id"
   belongs_to :parent, class_name: "Category"
 
 	has_many :ingredients
-
-	def hierarchy_link(link_type = nil, category, item, classes)
-		if link_type == :store
-			if children.any?
-				helpers.link_to name, store_category_path(item), class: classes
-			else
-				helpers.link_to name, new_store_category_ingredient_path(category), class: classes
-			end
-		else
-			if children.any?
-				helpers.link_to name, category_path(item), class: classes
-			else
-				helpers.link_to name, category_ingredients_path(item), class: classes
-			end
-		end
-	end
-
-	# include #link_to for use in #hierarchy_link
-	def helpers
-		ActionController::Base.helpers
-	end
 end

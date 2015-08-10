3d1ab5340351a10d3436eb763ed5e23fb9111415
Michael Yanchar <michael.yanchar@gmail.com>
2015-08-10 12:20:37 -0700

    Styling for admin/location and admin/category controls. Fixed improperly linked stylesheets and javascript errors.

--- app/controllers/relocate/ingredients_controller.rb
+++ app/controllers/relocate/ingredients_controller.rb
@@ -2,6 +2,7 @@ class Relocate::IngredientsController < ApplicationController
   def change_location
     ingredient = Ingredient.find(params[:id])
     ingredient.update(location_id: params[:location_id])
+    flash[:notice] = "LOCATION SET"
     redirect_to ingredient_path(ingredient)
   end
 end

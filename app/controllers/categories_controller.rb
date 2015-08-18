3f6d4560d19caddc89681bed4536a923c66b22e4
garedean <garrettdolson@gmail.com>
2015-08-18 12:16:02 -0700

    Breadcrumb all the things

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   def show
-    @food_category = Category.find(params[:id])
-    @food_category_children = @food_category.children
+    @category = Category.find(params[:id])
   end
 end

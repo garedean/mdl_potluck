77c3b505086d6814998ca9e9abe5574b961365c7
Michael Yanchar <michael.yanchar@gmail.com>
2015-08-18 14:45:51 -0700

    Removed erroring link+to

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   get '/recentlylogged' => 'ingredients#recentlylogged'
   get '/ingredients/expiring', to: 'ingredients#expiring_soon'
   get '/ingredients/unarranged', to: 'ingredients#unarranged'
-  get '/ingredients/search', to: 'ingredients#search'
 
   resources :locations, :categories
   resource :cart

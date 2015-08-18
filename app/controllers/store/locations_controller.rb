3cb7843643bfbeaceaa8057d64e7590add854b26
garedean <garrettdolson@gmail.com>
2015-08-18 12:20:39 -0700

    Fix bug where controller was calling nonexistent superclass method

--- app/controllers/store/locations_controller.rb
+++ app/controllers/store/locations_controller.rb
@@ -1,6 +1,5 @@
 class Store::LocationsController < LocationsController
   def index
-    super
     @category = params[:category_id]
     @locations = Location.where(ancestry: nil).order(:name)
     @hide_no_location_option = true if params[:hide_no_location_option]

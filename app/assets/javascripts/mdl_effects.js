d705a2c81c40a89f367bc17f3686ce3652c5f6d4
garedean <garrettdolson@gmail.com>
2015-08-19 18:33:33 -0700

    Add ripple effect to list items

---
+++ app/assets/javascripts/mdl_effects.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  $('.ripple').on('mousedown', function (event) {
+    //event.preventDefault();
+
+    var $div = $('<div>'),
+        btnOffset = $(this).offset(),
+    		xPos = event.pageX - btnOffset.left,
+    		yPos = event.pageY - btnOffset.top;
+
+    var $ripple = $div.addClass('ripple-effect');
+
+    $ripple.css("height", $(this).height());
+    $ripple.css("width", $(this).height());
+    $div
+      .css({
+        top: yPos - ($ripple.height()/2),
+        left: xPos - ($ripple.width()/2),
+        background: $(this).data("ripple-color")
+      })
+      .appendTo($(this));
+
+      window.setTimeout(function(){
+        $div.remove();
+      }, 1000);
+  });
+});

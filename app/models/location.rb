1c277a4e0fb37139a24d496195b892e964cdf64a
garedean <garrettdolson@gmail.com>
2015-08-21 11:51:56 -0700

    Add styling to find by category workflow

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,6 +6,15 @@ class Location < ActiveRecord::Base
   validates_presence_of :name
   has_ancestry
 
+  # Returns a tree-like string for locations: Freezer > Back > Top > Right
+  def tree_format
+    output = []
+    ancestors.each do |location|
+      output << location.name
+    end
+    output.join(" > ")
+  end
+
   private
 
     def remove_child_associations
